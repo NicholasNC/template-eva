5eb9b20d38d105711fd30f22165a1b0a458ff3a5
wuqinfa <wuqinfa@37.com>
2021-12-26 17:07:29 +0800

    chore: 完成插入头像的功能

--- src/insert/TextGenerator.ts
+++ src/insert/TextGenerator.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-26 14:18:29
- * @LastEditTime: 2021-12-26 14:49:20
+ * @LastEditTime: 2021-12-26 17:06:39
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /template-eva/src/insert/TextGenerator.ts
@@ -40,13 +40,13 @@ export default abstract class TextGenerator {
 
     const text = this.generate(args);
 
-    // if (text) {
-    //   editor.edit(edit =>
-    //     editor.selections.forEach(selection => {
-    //       edit.delete(selection);
-    //       edit.insert(selection.start, text);
-    //     })
-    //   );
-    // }
+    if (text) {
+      editor.edit(edit =>
+        editor.selections.forEach(selection => {
+          edit.delete(selection);
+          edit.insert(selection.start, text);
+        })
+      );
+    }
   }
 }
