1568d1e1c536fe1bc511cebdbc631429e266ffad
wuqinfa <wuqinfa@37.com>
2021-12-26 17:02:15 +0800

    choer: 修改细节

--- src/insert/AvatarGenerator.ts
+++ src/insert/AvatarGenerator.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-26 14:21:54
- * @LastEditTime: 2021-12-26 15:19:36
+ * @LastEditTime: 2021-12-26 16:47:16
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /template-eva/src/insert/AvatarGenerator.ts
@@ -17,13 +17,12 @@ export default class AvatarGenerator extends TextGenerator {
   
   protected generate(): string | undefined {
     const config = vscode.workspace.getConfiguration('template-eva');
-    const src: string | undefined = config.get('avatar');
-    // if (src) {
-    //   switch (src) {
-    //     case "pravatar":
-    //       return `http://i.pravatar.cc/${size}`;
-    //   }
-    // }
-    vscode.window.showErrorMessage('eee');
+    const avatar: string | undefined = config.get('avatar');
+    
+    if (avatar) {
+      return avatar;
+    }
+
+    vscode.window.showErrorMessage('no avatar in config"');
   }
 }
