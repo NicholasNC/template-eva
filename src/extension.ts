4e4b2b89f1f58d2f9dd6838eb5652fbb34e2f9d4
wuqinfa <wuqinfa@37.com>
2021-12-26 20:38:11 +0800

    chore: 完成图片插入

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-12-26 14:11:31
- * @LastEditTime: 2021-12-26 14:59:06
+ * @LastEditTime: 2021-12-26 20:36:23
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /template-eva/src/extension.ts
  */
 import * as vscode from 'vscode';
 
-import AvatarGenerator from './insert/AvatarGenerator';
+import ImgGenerator from './insert/ImgGenerator';
 
 
 
@@ -18,11 +18,18 @@ import AvatarGenerator from './insert/AvatarGenerator';
  *    只会在你的插件激活时执行一次
  */
 export function activate(context: vscode.ExtensionContext) {
-  const avatar = new AvatarGenerator();
+  const avatar = new ImgGenerator('avatar');
+  const image = new ImgGenerator('image');
 
   context.subscriptions.push(
     vscode.commands.registerCommand('template-eva.insertAvatar', () => {
       avatar.execute();
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('template-eva.insertImage', () => {
+      image.execute();
     })
   );
 }
--- src/insert/ImgGenerator.ts
+++ src/insert/ImgGenerator.ts
@@ -1,28 +1,32 @@
 /*
  * @Author: your name
  * @Date: 2021-12-26 14:21:54
- * @LastEditTime: 2021-12-26 16:47:16
+ * @LastEditTime: 2021-12-26 20:38:02
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: /template-eva/src/insert/AvatarGenerator.ts
+ * @FilePath: /template-eva/src/insert/ImgGenerator.ts
  */
 import * as vscode from 'vscode';
 
 import TextGenerator from './TextGenerator';
 
-export default class AvatarGenerator extends TextGenerator {
-  constructor() {
+export default class ImgGenerator extends TextGenerator {
+  private configKey: string;
+
+  constructor(configKey: string) {
     super(false); // 插入头像，不需要让用户再输入参数
+
+    this.configKey = configKey;
   }
   
   protected generate(): string | undefined {
     const config = vscode.workspace.getConfiguration('template-eva');
-    const avatar: string | undefined = config.get('avatar');
+    const url: string | undefined = config.get(this.configKey);
     
-    if (avatar) {
-      return avatar;
+    if (url) {
+      return url;
     }
 
-    vscode.window.showErrorMessage('no avatar in config"');
+    vscode.window.showErrorMessage(`no ${this.configKey} in config`);
   }
 }
