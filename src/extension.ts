e8a25969fb950055387496044502df8f3b97dbed
yutucc <369724377@qq.com>
2021-12-29 22:38:40 +0800

    chore: 完成乱数假文的开发

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-26 14:11:31
- * @LastEditTime: 2021-12-29 22:33:01
+ * @LastEditTime: 2021-12-29 22:36:06
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /template-eva/src/extension.ts
@@ -34,7 +34,7 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('template-eva.insertImage', () => {
+    vscode.commands.registerCommand('template-eva.insertLorem', () => {
       lorem.execute();
     })
   );
