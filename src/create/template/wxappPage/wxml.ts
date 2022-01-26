985437f3442a3ee334922cb256702fc4b993c9f9
yutucc <369724377@qq.com>
2022-01-26 22:21:01 +0800

    chore: 添加模板

---
+++ src/create/template/wxappPage/wxml.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: wuqinfa
+ * @Date: 2022-01-01 14:28:34
+ * @LastEditTime: 2022-01-26 22:20:46
+ * @LastEditors: wuqinfa
+ * @Description: 
+ */
+
+export default `
+<!--pages/index.wxml-->
+<text>pages/index.wxml</text>
+`;
