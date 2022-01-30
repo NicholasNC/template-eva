162fac377fe26f79f3e21dc4625ea157abf32fb5
yutucc <369724377@qq.com>
2022-01-30 12:03:37 +0800

    chore: 添加 log 的生成

--- src/console/ConsoleGenerator.ts
+++ src/console/ConsoleGenerator.ts
@@ -1,21 +1,86 @@
 /*
  * @Author: wuqinfa
  * @Date: 2022-01-29 10:17:18
- * @LastEditTime: 2022-01-29 10:24:39
+ * @LastEditTime: 2022-01-30 11:54:28
  * @LastEditors: wuqinfa
  * @Description: 
  */
 import {
   workspace,
   window,
+  commands,
   
-  QuickPickItem,
   Uri,
+  TextEditor,
 } from 'vscode';
+import lodash from 'lodash';
 
 export default class ConsoleGenerator {
   async execute(uri: Uri) {
-    console.log('object :>> ', 1111);
+    const editor = window.activeTextEditor;
+
+    if (!editor) {
+        window.showErrorMessage('还没打开编辑文档，不能插入 log');
+        return;
+    }
+
+    const selections = editor.selections;
+
+    console.log('selections :>> ', selections);
+
+    try {
+      await commands.executeCommand('editor.action.addSelectionToNextFindMatch');
+
+      const allSelText: string[] = this.getAllSelText(editor);
+      const endOfBlock = this.getEndOfBlock(editor);
+      
+
+      console.log('allSelText :>> ', allSelText);
+      console.log('endOfBlock :>> ', endOfBlock);
+    } catch (error) {
+      
+    }
+  }
+
+  private getAllSelText(editor: TextEditor): string[] {
+    const result: string[] = [];
+
+    editor.selections.forEach((item) => {
+      result.push(editor.document.getText(item));
+    });
+
+    return result;
+  }
+
+  private getEndOfBlock(editor: TextEditor): number[] {
+    const result: number[] = [];
+
+    const tempLines = editor.selections.map((item) => {
+      return item.start.line;
+    });
+    const allSelLines: number[] = lodash.orderBy(lodash.uniq(tempLines));
+    const length = allSelLines.length;
+
+    let tempLine: null | number = null;
+
+    allSelLines.forEach((line: number, index: number) => {
+      if (tempLine === null) {
+        tempLine = line;
+        return;
+      }
+
+      if (line !== (tempLine + 1)) {
+        result.push(tempLine);
+      }
+
+      tempLine = line;
+
+      if (index === (length - 1)) {
+        result.push(line);
+      }
+    });
+
+    return result;
   }
 
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2021-12-26 14:11:31
- * @LastEditTime: 2022-01-29 10:23:47
+ * @LastEditTime: 2022-01-30 12:01:24
  * @LastEditors: wuqinfa
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: /template-eva/src/extension.ts
+ * @Description: 
  */
 import * as vscode from 'vscode';
 
